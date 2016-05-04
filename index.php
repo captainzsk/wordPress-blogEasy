124574a1c5d34a103a2da87fe419f6e22f0c8693
lubin.chen <chenlubin@zhongan.com>
2016-05-04 17:16:42 +0800

    add

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 因为项目支持的是个人站长，而不是面向公司，所以请在php7下运行这份代码
+ * 建议在PHP5.6以上版本运行
  * @author Lubin Chen <newlubin@163.com>
  */
 
@@ -9,6 +9,7 @@ define('BASE_PATH', __DIR__.'/');
 function auto_load_function($className){
     include_once BASE_PATH. $className.'.php';
 }
+
 spl_autoload_register("auto_load_function");
 
 include_once BASE_PATH.'config.php';
