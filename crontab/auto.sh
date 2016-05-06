2e44864f3cf143d1bc445b383d41be8cb89c2fba
lubin.chen <chenlubin@zhongan.com>
2016-05-06 15:30:00 +0800

    add

--- crontab/auto.sh
+++ crontab/auto.sh
@@ -1,16 +1,16 @@
-
-
-if [ ! -d newlubin.github.io ];then 
+ 
+if [ ! -d blog ];then 
 #    git clone git@newlubin.github.io:newlubin/newlubin.github.io.git blog; 
     git clone `php getGit.php` blog; 
 fi; 
-
+ 
 cd blog; 
-
-git reset --hard HEAD;
-git pull;
-cp ../../blogHtml/* ./ -rf; 
-
-git add -A; 
-git commit -m "sync blog"; 
-git push origin master; 
+ 
+git reset --hard HEAD; 
+git pull; 
+cp ../../blogHtml/* ./ -rf;  
+ 
+git add -A;  
+git commit -m "sync blog";  
+git push origin master;  
+ 
