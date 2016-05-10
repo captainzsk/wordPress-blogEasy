637a72bfb6321cbd53917720d88426fe1fb708e6
lubin.chen <chenlubin@zhongan.com>
2016-05-10 11:13:43 +0800

    fix a bug of file name first char

--- config_example.php
+++ config_example.php
@@ -5,8 +5,8 @@ define('BLOG_SUB_TITLE', 'Make PHP programmer world happier');
 
 //博客的网址 
 define('URL_BLOG', 'http://blog.yioio.com/');
-//RSS2地址
-define('URL_RSS', URL_BLOG."rss2.xml");
+//RSS2名称
+define('RSS_NAME', "rss2.xml");
 //原来的域名 
 define('DOMAIN_REAL_OLD', 'treephp.yioio.com');
 //要被替换的域名 
